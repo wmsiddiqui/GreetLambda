4380c970464daacd4f5686e2a7c085cd21e2e335
wmsiddiqui <wmsiddiqui.6@gmail.com>
2018-01-07 21:59:11 -0600

    Initial Commit.

---
+++ deploy.ps1
@@ -0,0 +1,16 @@
+if (Test-Path "./deploy") {
+	Remove-Item -path "./deploy" -recurse
+}
+
+New-Item -Path ./deploy/bin -ItemType Directory -Force
+New-Item -Path ./deploy/compressed -ItemType Directory -Force
+dotnet publish "./TestLambdaProject/TestLambdaProject.csproj" --output "../deploy/bin"
+
+[Reflection.Assembly]::LoadWithPartialName( "System.IO.Compression.FileSystem" )
+[System.IO.Compression.ZipFile]::CreateFromDirectory("./deploy/bin", "./deploy/compressed/package.zip")
+
+aws s3 cp ./deploy/compressed/package.zip s3://greet-lambda/greet.zip
+
+aws --region us-east-1 cloudformation deploy --template-file "./Stack.json" --stack-name GreetStack
+
+Read-Host -Prompt "Press Enter to exit"
